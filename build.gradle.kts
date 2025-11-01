829366507fa43cafa0d0199951fcb32fa8854682
iHagoss <178996594+iHagoss@users.noreply.github.com>
2025-11-02 02:25:56 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id 'com.android.application' version '8.10.1' apply false
+    id 'org.jetbrains.kotlin.android' version '1.9.22' apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+import org.gradle.api.tasks.Delete
+
+tasks.register('clean', Delete) {
+    delete rootProject.getLayout().getBuildDirectory()
 }
